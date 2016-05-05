d2c9f50744de71c7398fa0123868ff5591c0d6c4
Aleksi Peebles <aleksi@iki.fi>
2016-05-05 17:13:32 +0300

    Add PatternDataNodeVisitor

--- src/aleksip/DataTransformPlugin/PatternLabListener.php
+++ src/aleksip/DataTransformPlugin/PatternLabListener.php
@@ -2,14 +2,17 @@
 
 namespace aleksip\DataTransformPlugin;
 
+use aleksip\DataTransformPlugin\Twig\PatternDataNodeVisitor;
 use PatternLab\Listener;
 use PatternLab\PatternData\Event;
+use PatternLab\PatternEngine\Twig\TwigUtil;
 
 class PatternLabListener extends Listener
 {
     public function __construct()
     {
         $this->addListener('patternData.codeHelperStart', 'runHelper');
+        $this->addListener('twigPatternLoader.customize', 'addNodeVisitor');
     }
 
     public function runHelper(Event $event)
@@ -17,5 +20,12 @@ class PatternLabListener extends Listener
         $options = $event->getOptions();
         $helper = new Helper($options);
         $helper->run();
+    }
+
+    public function addNodeVisitor()
+    {
+        $instance = TwigUtil::getInstance();
+        $instance->addNodeVisitor(new PatternDataNodeVisitor());
+        TwigUtil::setInstance($instance);
     }
 }
