c76b1191f6b55357b27ccf23e47a1cd198346e05
Aleksi Peebles <aleksi@iki.fi>
2018-01-22 15:17:38 +0200

    Add support for Twig namespace includes

--- src/aleksip/DataTransformPlugin/Twig/PatternDataNodeVisitor.php
+++ src/aleksip/DataTransformPlugin/Twig/PatternDataNodeVisitor.php
@@ -23,6 +23,27 @@ class PatternDataNodeVisitor extends \Twig_BaseNodeVisitor
         if ($node instanceof \Twig_Node_Include) {
             if ($node->hasNode('expr') && $node->getNode('expr')->hasAttribute('value')) {
                 $patternStoreKey = $node->getNode('expr')->getAttribute('value');
+
+                // Handle Twig namespace includes
+                if ($patternStoreKey[0] == '@') {
+                    $patternStoreKey = ltrim($patternStoreKey, '@');
+                    $lineageParts = explode('/', $patternStoreKey);
+                    $length = count($lineageParts);
+                    $patternType = $lineageParts[0];
+
+                    $patternName = $lineageParts[$length - 1];
+                    $patternName = ltrim($patternName, '_');
+                    $patternName = preg_replace('/^[0-9\-]+/', '', $patternName);
+
+                    $patternNameStripped = explode('.twig', $patternName);
+
+                    if (count($patternNameStripped) > 1) {
+                        $patternName = $patternNameStripped[0];
+                    }
+                    $patternName = str_replace('.', '-', $patternName);
+                    $patternStoreKey = $patternType . "-" . $patternName;
+                }
+
                 $data = $this->dt->getProcessedPatternSpecificData($patternStoreKey);
                 if ($node instanceof \Twig_Node_Embed) {
                     $dataNode = new PatternDataEmbedNode($node, $data);
